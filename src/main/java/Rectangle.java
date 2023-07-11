98fb8e08b5b2cc594506fcdf5c4599da40900c29
mikeycb96 <michaelsonbaptist@gmail.com>
2023-07-11 16:13:19 +0100

    added rectangle.java

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,24 @@
+public class Rectangle {
+
+    private int length;
+    private int width;
+    private boolean isRectangle;
+
+    public Rectangle(int length, int width){
+        this.length = length;
+        this.width = width;
+        this.isRectangle = true;
+    }
+
+    public int calculateArea() {
+        return length * width;
+    }
+    public boolean checkIfSquare() {
+        if (length == width) {
+            this.isRectangle = true;
+        } else this.isRectangle = false;
+
+    return this.isRectangle;}
+
+
+}
